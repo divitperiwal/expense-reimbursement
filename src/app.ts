a3c3e6e36e0086d67d2cc662a16fefeb9adac293
divitperiwal <divitperiwal@gmail.com>
2026-04-02 14:57:26 +0530

    chore: add error global error handling middleware in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { corsOptions } from './constants.js';
-import { sendSuccess } from './utils/helper/response.js';
+import { sendSuccess } from './utils/constants/response.js';
+import authRoutes from "./modules/auth/auth.route.js"
+import { errorHandler, notFound } from './middlewares/error.middleware.js';
 
 
 const app = express();
@@ -20,5 +22,11 @@ app.get('/health', (req, res) => {
     sendSuccess(res, 200, "Server is healthy", { timestamp: Date.now() });
 });
 
+app.use('/api/v1/auth', authRoutes);
+
+
+
+app.use(errorHandler);
+app.use(notFound);
 
 export default app;
