86631bb030a5fd576bb9ad93077d4dd72a0878b1
divitperiwal <divitperiwal@gmail.com>
2026-04-03 01:56:20 +0530

    chore: remove refresh token from jwt and add Zod error handling in middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,10 +1,13 @@
 import { sendError } from '@/utils/constants/response.js';
 import type { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
     if (error.name === 'ValidationError') return sendError(res, 400, 'Validation Error', error);
     if (error.name === 'UnauthorizedError') return sendError(res, 401, 'Unauthorized Access', error);
 
+    if (error instanceof ZodError)
+        return sendError(res, 400, error.issues[0].message || 'Validation Error', error.issues);
 
     return sendError(
         res,
--- src/utils/helper/jwt.ts
+++ src/utils/helper/jwt.ts
@@ -1,34 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
 
-if (!ACCESS_TOKEN_SECRET || !REFRESH_TOKEN_SECRET) {
+if (!ACCESS_TOKEN_SECRET) {
     throw new Error('JWT secrets are not defined in environment variables');
 }
 
 //Access Token
 export const signAccessToken = (payload: object): string => {
     return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
-        expiresIn: '15m',
+        expiresIn: '12h',
         algorithm: 'HS256'
     })
 }
 
 export const verifyAccessToken = (token: string): object => {
     return jwt.verify(token, ACCESS_TOKEN_SECRET) as object;
-}
-
-
-//Refresh Token
-
-export const signRefreshToken = (payload: object): string => {
-    return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
-        expiresIn: '3d',
-        algorithm: 'HS256'
-    })
-}
-
-export const verifyRefreshToken = (token: string): object => {
-    return jwt.verify(token, REFRESH_TOKEN_SECRET) as object;
 }
