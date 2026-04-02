5bf8c7d1b0af82a693f8325ca4cb117d9649e0b7
divitperiwal <divitperiwal@gmail.com>
2026-04-02 13:34:52 +0530

    fix: add dotenv config to get database url from environment

--- src/config/drizzle.config.ts
+++ src/config/drizzle.config.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from "@/database/schema/index.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectionString = process.env.DATABASE_URL || "";
 
