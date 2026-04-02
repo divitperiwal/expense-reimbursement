70388beae6f47b2d562fc142e2c9faa99dc1ae76
divitperiwal <divitperiwal@gmail.com>
2026-04-02 13:24:25 +0530

    feat: add drizzle configurations and connect with supabase

---
+++ src/config/drizzle.config.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from "@/database/schema/index.js";
+
+const connectionString = process.env.DATABASE_URL || "";
+
+const client = postgres(connectionString, {
+    max: 10,
+    ssl: "require",
+    idle_timeout: 30,
+    connect_timeout: 10
+})
+
+const db = drizzle(client, { schema })
+
+export default db;
