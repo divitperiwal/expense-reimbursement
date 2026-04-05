ad72cfd6e93ebc46e18f373cd345bf7a8d81f3fe
divitperiwal <divitperiwal@gmail.com>
2026-04-05 21:01:55 +0530

    refactor: remove redis from rate-limiter

--- src/middlewares/rate-limit.middleware.ts
+++ src/middlewares/rate-limit.middleware.ts
@@ -1,19 +1,10 @@
 import rateLimit from 'express-rate-limit';
-import { RedisStore } from 'rate-limit-redis';
-import redis from '@/config/redis.config.js';
-
-const createRedisStore = (prefix: string) =>
-    new RedisStore({
-        sendCommand: (...args: string[]) => redis.call(args[0], ...args.slice(1)) as Promise<any>,
-        prefix,
-    });
 
 export const apiRateLimiter = rateLimit({
     windowMs: 60 * 1000,
     max: 200,
     standardHeaders: true,
     legacyHeaders: false,
-    store: createRedisStore('rl:api:'),
     message: {
         success: false,
         statusCode: 429,
@@ -26,7 +17,6 @@ export const authRateLimiter = rateLimit({
     max: 20,
     standardHeaders: true,
     legacyHeaders: false,
-    store: createRedisStore('rl:auth:'),
     message: {
         success: false,
         statusCode: 429,
